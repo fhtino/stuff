3bee7a26f4ae681e75abb139a79e84763e2cae8c
Fabry <637401+fhtino@users.noreply.github.com>
2026-03-14 09:03:24 +0100

    netmq tests

---
+++ NetMQtests/PPClient/Client.cs
@@ -0,0 +1,39 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace PPClient
+{
+    internal class Client
+    {
+        public static void Main(string[] args)
+        {
+            string serverIP = args.Length> 0 ? args[0] : "localhost";
+
+            Console.WriteLine($"Connecting to server at {serverIP}");
+
+            var client = new PushSocket();
+            client.Options.SendHighWatermark = 100;  // to limit the number of messages in the output queue
+            client.Connect($"tcp://{serverIP}:7777");
+
+            int messageCounter = 0;
+            int failCounter = 0;
+            while (true)
+            {
+                var message = $"BODY_{DateTime.UtcNow.ToString("O")}_{messageCounter++}";
+                Console.Write($"Sending message [{message}]...");
+                //client.SendFrame(message); // blocking, if output queue is full
+                bool sendResult = client.TrySendFrame(message);  // does not block, but you can lose messages if output queue is full
+                Console.WriteLine($" sendResult={sendResult}");
+                if (!sendResult) failCounter++;
+                
+                Thread.Sleep(1000);
+                
+                if (messageCounter >= 5)
+                    break;                
+            }
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NetMQtests/PPServer/Server.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace PPServer
+{
+    internal class Server
+    {
+        public static void Main(string[] args)
+        {
+            int port = 7777;
+            Console.WriteLine($"Starting server on port {port}...");
+            var server = new PullSocket();
+            server.Bind($"tcp://*:{port}");
+            var poller = new NetMQPoller { server };
+            server.ReceiveReady += (sender, args) =>
+            {
+                var message = args.Socket.ReceiveFrameString();
+                Console.WriteLine($"Received message: [{message}]");
+            };
+            poller.RunAsync("ServerPoller", isBackgroundThread:true);
+            Console.WriteLine("Server is running.\nPress any key to exit.\n");
+            Console.ReadKey();
+        }
+    }
+}
